9bffd77c175e46d0158ae427cebbf24089590a8f
Kunmun <kunmun@aospa.co>
2024-02-25 13:15:10 +0530

    wlan: qcacmn: Fix buffer overflow in debug_dump_bytes
    /home/kunmun/phone2/kernel/kernel_platform/msm-kernel/../../vendor/qcom/opensource/wlan/qcacld-3.0/cmn/htc/htc_recv.c:48:4: error: 'snprintf' will always be truncated; specified size is 2, but format string expands to at least 5 [-Werror,-Wfortify-source]
       48 |                         A_SNPRINTF(byteOffsetStr, sizeof(byteOffset), "%4.4X",
          |                         ^
    ../../../vendor/qcom/opensource/wlan/qcacld-3.0/.qca6490/uapi/linux/osapi_linux.h:75:42: note: expanded from macro 'A_SNPRINTF'
       75 | #define A_SNPRINTF(buf, len, args ...)   snprintf(buf, len, args)
          |                                          ^
    /home/kunmun/phone2/kernel/kernel_platform/msm-kernel/../../vendor/qcom/opensource/wlan/qcacld-3.0/cmn/htc/htc_recv.c:57:3: error: 'snprintf' will always be truncated; specified size is 2, but format string expands to at least 5 [-Werror,-Wfortify-source]
       57 |                 A_SNPRINTF(byteOffsetStr, sizeof(byteOffset), "%4.4X",
          |                 ^
    ../../../vendor/qcom/opensource/wlan/qcacld-3.0/.qca6490/uapi/linux/osapi_linux.h:75:42: note: expanded from macro 'A_SNPRINTF'
       75 | #define A_SNPRINTF(buf, len, args ...)   snprintf(buf, len, args)
          |                                          ^
    2 errors generated.
    
    Change-Id: Icf7613a441d8cfa9ad986300a6fb85d7dd5f005b

--- wlan/qca-wifi-host-cmn/htc/htc_recv.c
+++ wlan/qca-wifi-host-cmn/htc/htc_recv.c
@@ -26,7 +26,7 @@
 void debug_dump_bytes(uint8_t *buffer, uint16_t length, char *pDescription)
 {
 	int8_t stream[60];
-	int8_t byteOffsetStr[10];
+	char byteOffsetStr[6];
 	uint32_t i;
 	uint16_t offset, count, byteOffset;
 
@@ -45,7 +45,7 @@ void debug_dump_bytes(uint8_t *buffer, uint16_t length, char *pDescription)
 		if (count == 16) {
 			count = 0;
 			offset = 0;
-			A_SNPRINTF(byteOffsetStr, sizeof(byteOffset), "%4.4X",
+			A_SNPRINTF(byteOffsetStr, sizeof(byteOffsetStr), "%4.4X",
 				   byteOffset);
 			A_PRINTF("[%s]: %s\n", byteOffsetStr, stream);
 			qdf_mem_zero(stream, 60);
@@ -54,7 +54,7 @@ void debug_dump_bytes(uint8_t *buffer, uint16_t length, char *pDescription)
 	}
 
 	if (offset != 0) {
-		A_SNPRINTF(byteOffsetStr, sizeof(byteOffset), "%4.4X",
+		A_SNPRINTF(byteOffsetStr, sizeof(byteOffsetStr), "%4.4X",
 			   byteOffset);
 		A_PRINTF("[%s]: %s\n", byteOffsetStr, stream);
 	}
